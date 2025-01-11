c976878a7ffa065c917dc6dad88922549c4a9c85
Prometheus <proust.bastien@gmail.com>
2025-01-11 18:41:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-print("Hello")
+import json
+
+def main():
+    with open("params.json", "r") as f:
+        params = json.load(f)
+
+    print("Paramètres reçus :", params)
+
+    print("Hello")
+if __name__ == "__main__":
+    main()
