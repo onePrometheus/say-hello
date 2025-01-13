088fd82e6b859c7fae5e9cb8b39aaa19534449a9
Prometheus <proust.bastien@gmail.com>
2025-01-13 19:59:55 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 def main():
     params = {}
@@ -9,7 +10,8 @@ def main():
         pass
 
     print("Paramètres reçus :", params)
-
+    print("Wait for 10 seconds")
+    time.sleep(10)
     print("Hello")
 if __name__ == "__main__":
     main()
